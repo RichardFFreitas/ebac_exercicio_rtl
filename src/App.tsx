3ae51786b92832a58d08db604b32f9297ab0a57d
Colding-Team <richardfelipelives@gmail.com>
2024-04-23 17:11:17 -0300

    resolvendo exercicio

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://images.tcdn.com.br/img/img_prod/460977/estatua_batman_batman_vs_superman_a_origem_da_justica_art_scale_escala_1_10_iron_studios_19861_1_20201211173301.png">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
